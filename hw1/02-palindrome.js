32076ba8b05d4b911d8f7f20ba7acc67b9b27780
Minh Ma <ducminh@pdx.edu>
2023-04-12 12:08:30 -0700

    Complete HW1

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,19 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("input", handleInput);
+
+function handleInput() {
+  const input = document.querySelector("input").value;
+  const result = document.getElementById("result");
+
+  let inputReversed = input.split("").reverse().join("");
+  if (inputReversed === input) {
+    document.getElementById("result").innerHTML = "Yes, this is a palindrome";
+    result.classList.add("text-success");
+    result.classList.remove("text-danger");
+  } else {
+    document.getElementById("result").innerHTML = "No.Try again";
+    result.classList.add("text-danger");
+    result.classList.remove("text-success");
+  }
+}

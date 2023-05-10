f78dc5e4a2679f7243e1919f30250e48cb231548
Minh Ma <ducminh@pdx.edu>
2023-05-10 04:24:12 -0700

    Finish Ex 01+02, format modification still needed for 01

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,22 @@
 // url for the Thrones API
-const url = 'https://thronesapi.com/api/v2/Characters';
+const url = "https://thronesapi.com/api/v2/Characters";
 
-fetch(url);
+fetch(url)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+
+    const section = document.querySelector("section");
+    section.insertAdjacentHTML("beforeend", '<div class="row"></div>');
+
+    data.forEach((character) => {
+      const card = `<div class="col-12 col-md-6 col-lg-3"><div class="card  text-center h-100"><img class="card-img-top h-100 p-2" src="${character.imageUrl}" ><div class="card-text"><p>${character.fullName}</p><p>${character.title}</p></div></div></div></div>`;
+      const row = document.querySelector(".row");
+      row.insertAdjacentHTML("beforeend", card);
+    });
+  })
+  .catch((error) => console.log(error));
+
+  
--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -29,24 +29,71 @@ const borderColors = [
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
 
-const renderChart = () => {
+async function fetchAPI () {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    data.forEach((character) => {
+      console.log(character.family);
+    });
+    return data;
+  }
+  catch (error) {
+    console.log("ERROR MESSAGE: ", error);
+  }
+}
+
+function countCharacterByHouse(characters){
+  const characterByHouse = {};
+  
+  //|| houseStringReplace.includes(Object.keys(characterByHouse).substr(0,4))
+  characters.forEach((character) => {
+    const house = character.family.toLowerCase();
+    const houseStringReplace = house.replace("house ", "");
+    const houseSubString = houseStringReplace.substring(0,4);
+    const matchingHouse =  Object.keys(characterByHouse).find(key => key.substring(0,4) === houseSubString);
+    if(matchingHouse){
+      characterByHouse[matchingHouse]++;
+    }
+    else{
+      characterByHouse[houseStringReplace] = 1;
+    }
+  })
+  return characterByHouse;
+}
+
+const renderChart = (data) => {
   const donutChart = document.querySelector('.donut-chart');
+  const labels = Object.keys(data);
+  const values = Object.values(data);
 
   new Chart(donutChart, {
     type: 'doughnut',
     data: {
-      labels: ['label', 'label', 'label', 'label'],
+      labels: labels,
       datasets: [
         {
-          label: 'My First Dataset',
-          data: [1, 12, 33, 5],
+          
+          data: values,
           backgroundColor: backgroundColors,
           borderColor: borderColors,
           borderWidth: 1,
         },
       ],
     },
+    options: {
+      legend: {
+        display: false
+      }
+    }
   });
 };
 
-renderChart();
+
+(async() =>  {
+  const characters = await fetchAPI();
+  const characterCountByHouse = countCharacterByHouse(characters);
+  renderChart(characterCountByHouse);
+})();
+// fetchAPI();
+// renderChart();
